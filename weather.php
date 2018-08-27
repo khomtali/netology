9ee259bd5403db38de90fbe17a218ef44456a69a
khomtali <42213097+khomtali@users.noreply.github.com>
2018-08-27 12:40:05 +0300

    Update weather.php

--- weather.php
+++ weather.php
@@ -5,7 +5,7 @@
     $region = "ru";
     $units = "metric";
     $url = "http://api.openweathermap.org/data/2.5/weather?q=$city,$region&units=$units&appid=ae7d5313e2d51d4fc6e4adfb20eda50e";
-    $data = @file_get_contents($url);
+    $data = file_get_contents($url);
     if ($data) {
         $dataJson = json_decode($data);
         $weather = $dataJson->weather;
