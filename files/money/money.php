fbc72b83e2f9162f2e6a911e5dec818aac075476
khomtali <42213097+khomtali@users.noreply.github.com>
2018-09-19 16:39:21 +0300

    Update money.php

--- files/money/money.php
+++ files/money/money.php
@@ -1,18 +1,16 @@
 <?php
     $csv = 'costlist.csv';
     if (isset($argv[1]) && isset($argv[2])) {
-        $file = fopen($csv, 'w');
+        $file = fopen($csv, 'a');
         if($file) {
             $row[] = date('Y-m-d');
             $row[] = $argv[1];
             $purchase = implode(' ', array_slice($argv, 2));
             $row[] = $purchase;
-            if(!empty(fputcsv($file, $row, ";"))) {
-                $expense = implode(', ',$row);
-                echo "Expense added: $expense\r\n";
-                fclose($file);
-            } else echo "Error! Disable to write in file.\r\n";
-        } else echo "Error! Access to write in .csv file denied.\r\n";
+            $expense = implode(', ',$row);
+            echo "Expense added: $expense\n";
+            fclose($file);
+        } else echo "Error! Access to write in .csv file denied.\n";
     } elseif(isset($argv[1]) && $argv[1] == '--today') {
         if(is_readable($csv)) {
             $file = fopen($csv, 'r');
@@ -21,8 +19,8 @@
             while(!empty($expense))
                 if($expense[0] === date('Y-m-d'))
                     $sum += $expense[1];
-            echo date('Y-m-d')." current expense is .$sum\r\n";
+            echo date('Y-m-d')." current expense is .$sum\n";
             fclose($file);
-        } else echo ".csv file with expenses is not exist!\r\n";
-    } else echo "Error! Arguments are not specified. Put the flag --today or run the script with arguments {price} and {purchase}\r\n";
+        } else echo ".csv file with expenses is not exist!\n";
+    } else echo "Error! Arguments are not specified. Put the flag --today or run the script with arguments {price} and {purchase}\n";
 ?>
