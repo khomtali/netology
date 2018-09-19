cb14a5f00a9747cc52400f42e4892c1ae4a33f16
khomtali <42213097+khomtali@users.noreply.github.com>
2018-09-19 17:18:27 +0300

    Update money.php

--- files/money.php
+++ files/money.php
@@ -1,15 +1,17 @@
 <?php
     $csv = 'costlist.csv';
     if (isset($argv[1]) && isset($argv[2])) {
-        $file = fopen($csv, 'a');
+        $file = fopen($csv, 'w');
         if($file) {
             $row[] = date('Y-m-d');
             $row[] = $argv[1];
             $purchase = implode(' ', array_slice($argv, 2));
             $row[] = $purchase;
+            fputcsv($file, $row, ";")));
             $expense = implode(', ',$row);
             echo "Expense added: $expense\n";
             fclose($file);
+            } else echo "Error! Disable to write in file.\n";
         } else echo "Error! Access to write in .csv file denied.\n";
     } elseif(isset($argv[1]) && $argv[1] == '--today') {
         if(is_readable($csv)) {
