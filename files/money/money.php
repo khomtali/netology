285eaf869b1ff7e2b52a9bde94f7b7c0a39f4311
khomtali <42213097+khomtali@users.noreply.github.com>
2018-09-19 16:31:31 +0300

    Create money.php

---
+++ files/money/money.php
@@ -0,0 +1,28 @@
+<?php
+    $csv = 'costlist.csv';
+    if (isset($argv[1]) && isset($argv[2])) {
+        $file = fopen($csv, 'w');
+        if($file) {
+            $row[] = date('Y-m-d');
+            $row[] = $argv[1];
+            $purchase = implode(' ', array_slice($argv, 2));
+            $row[] = $purchase;
+            if(!empty(fputcsv($file, $row, ";"))) {
+                $expense = implode(', ',$row);
+                echo "Expense added: $expense\r\n";
+                fclose($file);
+            } else echo "Error! Disable to write in file.\r\n";
+        } else echo "Error! Access to write in .csv file denied.\r\n";
+    } elseif(isset($argv[1]) && $argv[1] == '--today') {
+        if(is_readable($csv)) {
+            $file = fopen($csv, 'r');
+            $expense = fgetcsv($file, '1000', ";");
+            $sum = 0;
+            while(!empty($expense))
+                if($expense[0] === date('Y-m-d'))
+                    $sum += $expense[1];
+            echo date('Y-m-d')." current expense is .$sum\r\n";
+            fclose($file);
+        } else echo ".csv file with expenses is not exist!\r\n";
+    } else echo "Error! Arguments are not specified. Put the flag --today or run the script with arguments {price} and {purchase}\r\n";
+?>
